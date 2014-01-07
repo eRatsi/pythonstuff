4c5b75be7342ee247fee01e37617453e324aa964
Emmanuel <doublecasse@gmail.com>
2014-01-07 11:22:11 +0100

    Update main.py
    bug fixes

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def start(name):
     enter_house()
   elif "look around" in next or "explore" in next:
     explore()
-  else
+  else:
     dead("Your head was chopped off by the illusion of your indecision")
     
-def enter_house()
+def enter_house():
   print "You've entered my house. Watch out for the giant pink elephant in the corner"
   next = raw_input("Quick what do you do?> ")
   if "kill" in next or "shoot" in next or "throw" in next:
@@ -24,12 +24,12 @@ def enter_house()
   else:
     dead("You're no good at all.")
 
-def explore()
+def explore():
   print "You found a notebook on the ground near the swingset. Must be full of answers don't you think?"
   next = raw_input("your answer is?> ")
   if "yes" in next:
     dead("You just learnt that you are your own grandfather and your cousin is your sister but also your wife.")
-  else
+  else:
     print "You win because I'm bored to death so I'm gonna do something else"
     exit(0)
 
