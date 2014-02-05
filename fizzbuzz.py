ddc46b07f8e853a5823d49f255c6cb8f75f4b671
Emmanuel <doublecasse@gmail.com>
2014-02-05 10:37:38 +0100

    dded fizzbuzz to repositor

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(seq):
+	output = []
+	for i in range(1, seq, 1):
+		if i % 3 == 0:
+			output.append("fizz")
+		elif i % 5 == 0:
+			output.append("buzz")
+		else:
+			output.append(str(i))
+	return output
+
+def main():
+	print(", ".join(fizzbuzz(int(input("FizzBuzz: ")))))
+if __name__ == '__main__':
+		main()
